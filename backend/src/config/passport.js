20cb63741af7fd810c77420440e2b47e2d2f17fc
devwithaditya <rajadityafeb22@gmail.com>
2026-08-01 00:57:09 +0530

    healthcheck route added

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -5,6 +5,11 @@ const { refreshToken } = require("../controller/auth.controller")
 const jwt = require("jsonwebtoken")
 const { generateRandomWords } = require("../utils/utils")
 
+console.log("=== ENV CHECK ===");
+console.log("GOOGLE_CLIENT_ID exists:", !!process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET exists:", !!process.env.GOOGLE_CLIENT_SECRET);
+console.log("GOOGLE_REDIRECT_URI exists:", !!process.env.GOOGLE_REDIRECT_URI);
+
 passport.use(
     new GoogleStrategy(
         {
@@ -48,7 +53,7 @@ passport.use(
         }
     )
 )
-
+console.log("=== ENV CHECK ===");
 console.log("GOOGLE_CLIENT_ID exists:", !!process.env.GOOGLE_CLIENT_ID);
 console.log("GOOGLE_CLIENT_SECRET exists:", !!process.env.GOOGLE_CLIENT_SECRET);
 console.log("GOOGLE_REDIRECT_URI exists:", !!process.env.GOOGLE_REDIRECT_URI);

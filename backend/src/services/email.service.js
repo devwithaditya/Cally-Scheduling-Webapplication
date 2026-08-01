ee5328dcbd02c7ff5ca6cce0da3cdb04cabb3491
devwithaditya <rajadityafeb22@gmail.com>
2026-08-01 09:59:26 +0530

    fix-email-service

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -7,16 +7,13 @@ const nodemailer = require("nodemailer")
 
 //create transporteer
 const transporter = nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    port:587,
-    secure:false,
-    auth:{
-        user:process.env.EMAIL_SERVICE_USER,
-        pass:process.env.EMAIL_SERVICE_PASS
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_SERVICE_USER,
+        pass: process.env.EMAIL_SERVICE_PASS,
     },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
 });
 
 (async () => {
